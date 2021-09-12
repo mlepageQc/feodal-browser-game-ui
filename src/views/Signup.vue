e2662c128b112d746016f4497d59caa2f73b92ab
Maxime Lepage <maximelepage2@gmail.com>
2021-09-12 12:45:45 -0400

    Removed spinner for now

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="signup">
 		<div>
-			<label for="userName">Email</label>
-			<input v-model="userName" name="userName" />
+			<label for="userName">Username</label>
+			<input id="userName" v-model="userName" />
 		</div>
 
 		<div>
 			<label for="email">Email</label>
-			<input v-model="email" name="email" />
+			<input id="email" v-model="email" />
 		</div>
 
 		<div>
 			<label for="password">Password</label>
-			<input v-model="password" type="password" name="password" />
+			<input id="password" v-model="password" type="password" />
 		</div>
 
 		<div>
 			<label for="passwordConfirmation">Password confirmation</label>
-			<input v-model="passwordConfirmation" type="password" name="passwordConfirmation" />
+			<input id="passwordConfirmation" v-model="passwordConfirmation" type="password" />
 		</div>
 		
 		<button type="submit" @click="createUser">Create user</button>
