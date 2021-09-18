aa8a246b1a784cd576bfacb6bec8f87983a60b3d
Maxime Lepage <maximelepage2@gmail.com>
2021-09-18 11:35:38 -0400

    Fixing user create params

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,9 +1,11 @@
 import axios from '@/api/Axios'
 
 export function createUser (name: string, email: string, password: string, passwordConfirmation: string) {
-	return axios.post('/users', {
-		user: {
-			name, email, password, password_confirmation: passwordConfirmation
+	return axios.post('/users', {}, {
+		params: {
+			user: {
+				name, email, password, password_confirmation: passwordConfirmation
+			}
 		}
 	})
 }
