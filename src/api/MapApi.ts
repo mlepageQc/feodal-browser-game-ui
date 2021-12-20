a67c252bbea0639e4abe3134f6e3558ff0f9518b
Maxime Lepage <maximelepage2@gmail.com>
2021-12-19 22:38:22 -0500

    Cleaned unused minimap + refactor a bit

--- src/api/MapApi.ts
+++ src/api/MapApi.ts
@@ -1,22 +1,6 @@
 import axios from '@/api/Axios'
-import { 
-	CoordinatesSet, 
-	ZoomLevel 
-} from '@/lib/map/types'
 import { ImageParams } from '@/types/ImageParams'
 
 export function fetchMapBase64Image (params: ImageParams) {
 	return axios('/map', { params })
-}
-
-export function fetchBuildingsBase64Image () {
-	return axios('/buildings')
-}
-
-export function fetchTileInformation (coordinates: CoordinatesSet) {
-	return axios('/tiles', { params: { coordinates } })
-}
-
-export function fetchMinimapBase64Image (zoomLevel: ZoomLevel) {
-	return axios('/minimap', { params: { zoomLevel } })
 }
--- src/config/Map.ts
+++ src/config/Map.ts
@@ -1,2 +1,3 @@
 export const MAP_SIZE = 8192
 export const MAP_IMAGE_SIZE = 1024
+export const TILE_SIZE = 64
